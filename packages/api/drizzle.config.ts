454e324a82081f7fa9b857a0863e09e016363645
jezpoz <jesfor94@gmail.com>
2024-05-01 16:56:40 +0200

    a little cleanup

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,10 +1,9 @@
-import "dotenv/config"
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: './src/db/schema/index.ts',
-  out: './src/db/drizzle',
-  driver: 'pg',
+  schema: "./src/db/schema/index.ts",
+  out: "./src/db/drizzle",
+  driver: "pg",
   dbCredentials: {
     connectionString: String(process.env.DB_URL),
   },
